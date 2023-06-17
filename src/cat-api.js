9ebc442ed0a5fc91d310de03cfbcc57c0f2e9e5f
OLEKSANDR KUROCHKIN <kurochkinac@gmail.com>
2023-06-17 11:26:51 +0300

    Done 1

---
+++ src/cat-api.js
@@ -0,0 +1,38 @@
+const apiFull = 'https://api.thecatapi.com/v1/breeds';
+const apiSearch = 'https://api.thecatapi.com/v1/images/search';
+const API_KEY =
+  'live_J1c3ReKXpbII84HmPX4RyPejOXkcqD0fyEejmLwc7SlwdX9GizKFc0O7vazMR0yY';
+
+  export function fetchBreeds() {
+  return fetch(apiFull, {
+    headers: {
+      'x-api-key': API_KEY,
+    },
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .catch(error => {
+      console.warn(error);
+    });
+}
+
+export function fetchCatByBreed(breedId) {
+  return fetch(`${apiSearch}?breed_ids=${breedId}`, {
+    headers: {
+      'x-api-key': API_KEY,
+    },
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .catch(error => {
+      console.warn(error);
+    });
+}
